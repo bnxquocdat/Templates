5f1aca43c2ae89dc3d18cf4f3b34451398f358b4
bnxquocdat <truongquocdat8282@gmail.com>
2023-09-04 22:52:56 +0700

    thuat toan

--- thuattoan/binanypow.cpp
+++ thuattoan/binanypow.cpp
@@ -4,24 +4,15 @@ using ll = long long;
 #define ull unsigned long long 
 const int mod = 1000000;
 const int MOD = 1e9+7;
-unsigned long long binpoww(long long a, long long b) {
+unsigned long long binpow(long long a, long long b) {
     unsigned long long res = 1;
     while (b > 0) {
         if (b & 1)
-            res = res * a;
-        a = a * a;
+            res = (res * a) % MOD;
+        a = (a * a) % MOD;
         b >>= 1;
     }
-    return res;
-}
-ull binpow(long long a, long long b) {
-    if (b == 0)
-        return 1;
-    ull res = binpow(a, b / 2);
-    if (b % 2)
-        return res * res * a;
-    else
-        return res * res;
+    return res % MOD;
 }
 ull binarypow(ull n , ull m) 
 {
